4ac7e98cd7655946898a5ec75296fc1767617d9c
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-13 23:25:25 +0100

    Refactor project name and update package files

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,12 @@ import "tailwindcss/tailwind.css";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { ActiveTabContextProvider } from "../context/ActiveTab";
 
 function App({ Component, pageProps }) {
   return (
-    <ActiveTabContextProvider>
+    <>
       <Head>
-        <title>SynergySells</title>
+        <title>StyleMatch</title>
       </Head>
       <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
@@ -25,7 +24,7 @@ function App({ Component, pageProps }) {
           }
         </div>
       </div>
-    </ActiveTabContextProvider>
+    </>
   );
 }
 
