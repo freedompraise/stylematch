a21cd9827d9a6081e602754729640366476fbd41
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-13 23:25:33 +0100

    Refactor about page and privacy policy
    - Refactor about page to rename project from SynergySells to StyleMatch and update project description and features.
    - Update privacy policy page to reflect the new project name and add introductory content.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**.{js,ts,jsx,tsx}",
-    "./components/**.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**.{js,ts,jsx,tsx}", "./components/**.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Bricolage", "sans-serif"],
+        heading: ["Grotesque", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
