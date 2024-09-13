c384e8c04182f6adf2e00d6fdc29daecd1a32f8a
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-13 23:51:58 +0100

    Add favicon to improve website branding

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ function App({ Component, pageProps }) {
           name="description"
           content="Grow your fashion business, one sale at a time"
         />
+        <link rel="shortcut icon" href="/favicon.jpg" />
       </Head>
       <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
