0e821a34f9fe5a6e33eaa028a9397eb91fe0a5a5
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-18 10:39:10 +0100

    Refactor vendor navbar to include user profile dropdown menu and improve responsiveness

--- components/Vendor/Navbar.js
+++ components/Vendor/Navbar.js
@@ -8,31 +8,29 @@ const Navbar = ({ vendorName }) => {
       <div className="flex justify-between items-center">
         <h1 className="text-xl font-bold text-blue-500">Vendor Dashboard</h1>
         <div className="relative inline-block text-left">
-          <div>
-            <button
-              type="button"
-              className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-              id="menu-button"
+          <button
+            type="button"
+            className="flex justify-center items-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 md:hidden"
+            id="menu-button"
+          >
+            <FontAwesomeIcon icon={faUserCircle} className="mr-2" />
+            {vendorName}
+            <svg
+              className="-mr-1 ml-2 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
             >
-              <FontAwesomeIcon icon={faUserCircle} className="mr-2" />
-              {vendorName}
-              <svg
-                className="-mr-1 ml-2 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
           <div
-            className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none md:static md:block md:w-auto" // Show on medium screens and larger
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="menu-button"
