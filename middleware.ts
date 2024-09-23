9d5433ff4a42aec86be4c85d6d1d8ba95ab12395
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-23 05:30:14 +0100

    Refactor middleware to simplify logic and remove unnecessary code

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,5 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const { pathname } = req.nextUrl;
-  const hostname = req.headers.get("host") || "";
-  const subdomain = hostname.split(".")[0];
-
-  if (subdomain && subdomain !== "www" && pathname === "/") {
-    return NextResponse.rewrite(new URL(`/${subdomain}`, req.url));
-  }
+  return NextResponse.next();
 }
