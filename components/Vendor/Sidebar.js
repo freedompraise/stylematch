749d6c93313ea0e5510f4b9a4cf74c70c44d8ee7
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-23 10:17:05 +0100

    Refactor Sidebar component to include toggle functionality for the sidebar

--- components/Vendor/Sidebar.js
+++ components/Vendor/Sidebar.js
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Sidebar = () => {
+const Sidebar = (isOpen, toggleSidebar) => {
   return (
-    <div className="w-64 bg-white shadow-md h-screen fixed left-0 top-0">
+    <div className="w-64 bg-white shadow-md h-screen fixed left-0 top-0 z-50 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-200 ease-in-out">
       <div className="p-4 bg-blue-500">
         <h2 className="text-white text-2xl font-bold">StyleMatch</h2>
       </div>
+      <button
+        onClick={toggleSidebar}
+        className="text-yellow-500 focus:outline-none"
+      >
+        <FontAwesomeIcon icon="times" />
+      </button>
       <ul className="space-y-2 pt-4">
         <li>
           <Link
--- layouts/VendorLayout.js
+++ layouts/VendorLayout.js
@@ -1,19 +1,19 @@
+import { useState } from "react";
 import Navbar from "../components/Vendor/Navbar";
 import Sidebar from "../components/Vendor/Sidebar";
 
 const VendorLayout = ({ children }) => {
-  const vendorName = "Vendor Name";
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
-    <div
-      className="flex 
-        flex-col 
-        h-screen 
-        bg-gray-100"
-    >
-      <Navbar vendorName={vendorName} />
-      <div className="flex flex-1">
-        <Sidebar />
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className="flex-1 flex flex-col">
+        <Navbar toggleSidebar={toggleSidebar} />
         <main className="flex-1 p-4">{children}</main>
       </div>
     </div>
